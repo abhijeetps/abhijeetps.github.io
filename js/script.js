9ce37472978c2cf8c88da72c1294b7c250f60f23
Abhijeet Singh <aps.120797@gmail.com>
2017-10-16 23:55:03 +0530

    Updated Typed.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 var typed = new Typed('#tag-line', {
     strings: ["Developer.^500 Quick Learner.^500 Neophile."],
     typeSpeed: 10,
+    startDelay: 1000,
     fadeOut: true,
     fadeOutClass: 'typed-fade-out',
     fadeOutDelay: 500
