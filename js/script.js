43b33ef5fbce013171ff08451364aabda9ee3bcc
Abhijeet Singh <aps.120797@gmail.com>
2018-03-19 22:15:30 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed('#tag-line', {
-    strings: ["Developer.^500Who Dreams.^500 Achieves. ^500;^100)"],
+    strings: ["Developer.^500 Dreamer.^500 Achiever. ^500;^100)"],
     typeSpeed: 10,
     startDelay: 1000,
     fadeOut: true,
